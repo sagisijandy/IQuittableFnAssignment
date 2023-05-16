588e43441dd9fe3a2d23ac5dbbcc2037d5362048
sagisijandy <98680878+sagisijandy@users.noreply.github.com>
2023-05-16 15:17:26 -0700

    Add project files.

---
+++ IQuittableFnAssignment/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Employee : IQuittable
+{
+    public string Name { get; set; }
+
+    public void Quit()
+    {
+        Console.WriteLine("Employee is quitting the job.");
+        // Any additional logic related to quitting can be added here
+    }
+}
---
+++ IQuittableFnAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+// Define the IQuittable interface with a Quit() method
+public interface IQuittable
+{
+    void Quit();
+}
+
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // Create an object of type IQuittable using polymorphism
+        IQuittable quittableEmployee = new Employee();
+
+        // Call the Quit() method on the object
+        quittableEmployee.Quit();
+
+        // Wait for user input to exit
+        Console.ReadLine();
+    }
+}
+
+
+
+
+
+
+
